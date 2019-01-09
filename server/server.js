7eef4bb6134aacbe6ac2e91609bf2dc012d92bbd
popescuangela1 <popescuangela1@yahoo.com>
2019-01-09 15:59:18 +0200

    Event hendler socket io

--- server/server.js
+++ server/server.js
@@ -1,20 +1,26 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 var publicPath = path.join(__dirname, '../public');
 
 var app = express();
-var port = process.env.PORT || 3000;
-
+var server = http.createServer(app);
+var io = socketIO(server);
 // set the middleware the public file path
 app.use(express.static(publicPath));
 
-app.listen(port, ()=> {
+io.on('connection', (socket)=>{
+    console.log('New user connected');
+    socket.on('disconnect', (socket)=>{
+        console.log('The user disconnected');
+    });
+});
+
+var port = process.env.PORT || 3000;
+server.listen(port, ()=> {
     console.log(`Started on ${port} port!`);
 });
 
-module.exports = {app}
-
-
-
-
+module.exports = {server}
